dd4823bc4ceb0616c587790569bb50ccbf35381c
hitode909 <hitode909@gmail.com>
2009-11-12 17:16:52 +0900

    user references

--- spec/model/channel_spec.rb
+++ spec/model/channel_spec.rb
@@ -9,6 +9,8 @@ describe MessageQueue::Channel do
     @ch_eu = load("message_queue/channel__euro")
     @session_a_ch_jp = load("message_queue/session__a_ch_jp")
     @session_b_ch_jp = load("message_queue/session__b_ch_jp")
+    @user_a = load("message_queue/user__a")
+    @user_b = load("message_queue/user__b")
   end
 
   it 'has user class' do
@@ -21,8 +23,15 @@ describe MessageQueue::Channel do
 
   it 'may has sessions' do
     @ch_eu.sessions.should be_empty
-    @ch_jp.sessions.length.should == 2
+    @ch_jp.sessions.length.should == 3
     @ch_jp.sessions.should include @session_a_ch_jp
     @ch_jp.sessions.should include @session_b_ch_jp
+  end
+
+  it 'may has users' do
+    @ch_eu.users.should be_empty
+    @ch_jp.users.length.should == 2
+    @ch_jp.users.should include @user_a
+    @ch_jp.users.should include @user_b
   end
 end
--- spec/model_helper.rb
+++ spec/model_helper.rb
@@ -1,4 +1,10 @@
+# -*- coding: utf-8 -*-
 require 'model/init'
 require 'fixture_dependencies'
 require 'fixture_dependencies/rspec/sequel'
 FixtureDependencies.fixture_path = File.dirname(__FILE__) + '/fixtures'
+
+# XXX: ロードしてないとhas_manyのカウントが変になったりする……
+["message_queue/channel","message_queue/session","message_queue/user"].each {|s|
+  FixtureDependencies.load s
+}
