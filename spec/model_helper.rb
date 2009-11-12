44c0e7dfd2da2d1b69edfde80b602f37b2e6c22a
hitode909 <hitode909@gmail.com>
2009-11-12 17:23:28 +0900

    fixture auto loadやめる(ここでロードするとDBに残ってしまう)

--- spec/model_helper.rb
+++ spec/model_helper.rb
@@ -4,7 +4,7 @@ require 'fixture_dependencies'
 require 'fixture_dependencies/rspec/sequel'
 FixtureDependencies.fixture_path = File.dirname(__FILE__) + '/fixtures'
 
-# XXX: ロードしてないとhas_manyのカウントが変になったりする……
-["message_queue/channel","message_queue/session","message_queue/user"].each {|s|
-  FixtureDependencies.load s
-}
+# # XXX: ロードしてないとhas_manyのカウントが変になったりする……
+# ["message_queue/channel","message_queue/session","message_queue/user"].each {|s|
+#   FixtureDependencies.load s
+# }
