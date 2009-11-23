d6f7f00862de15bb787b516d33be1f161568eecf
hitode909 <hitode909@gmail.com>
2009-11-23 20:07:47 +0900

    cui client

---
+++ cui_client.rb
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+require "net/http"
+require "uri"
+require "pp"
+require "json"
+require 'open-uri'
+
+res = Net::HTTP.post_form(URI.parse('http://localhost:7000/api/session/login'),
+  {:name => ARGV.shift, :password => ARGV.shift, :channel => ARGV.shift})
+session = JSON.parse res.body
+pp session
+
+puts "http://localhost:7000/api/session/get?session=#{session['session']['random_key']}"
+loop do
+  message = JSON.parse open("http://localhost:7000/api/session/get?session=#{session['session']['random_key']}&timeout=60").read
+  pp message
+end
