90d0231f43242ee4168d89c26f9cf5cd0d17c84d
hitode909 <hitode909@gmail.com>
2009-11-23 20:52:44 +0900

    repository with '/'

--- controller/repository.rb
+++ controller/repository.rb
@@ -1,5 +1,9 @@
 class RepositoryController < Controller
-  def index(path)
+  def index(*path)
+    path = path.join('/')
     @repos = Vcs::Repository.find(:path => path)
+    unless @repos
+      respond 'not found', 404
+    end
   end
 end
